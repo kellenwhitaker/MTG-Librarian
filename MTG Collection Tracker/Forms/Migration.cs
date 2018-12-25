5c7d586fd4d608eafd07ce2d72778f5b148e466a
Kellen Whitaker <helenacomputers@gmail.com>
2018-12-25 03:20:08 -0700

    got rid of KW's old migration code

--- MTG Collection Tracker/Forms/Migration.cs
+++ MTG Collection Tracker/Forms/Migration.cs
@@ -6,31 +6,8 @@ using System.Xml.Linq;
 
 namespace MTG_Librarian
 {
-    public partial class MainForm : Form
+    public partial class Migration
     {
-        private void AddPrices()
-        {
-            XElement root = XElement.Load("TCG_Player__Medium_.xml");
-            var results = root.Element("list").Elements("mc");
-            using (var context = new MyDbContext())
-            {
-                foreach (var result in results)
-                {
-                    int id = Convert.ToInt32(result.Element("id").Value);
-                    double price = Convert.ToDouble(result.Element("dbprice").Value);
-                    var card = from c in context.Catalog
-                               where c.multiverseId == id
-                               select c;
-                    if (card != null && card.Count() > 0)
-                        card.First().OnlinePrice = price;
-                }
-                context.SaveChanges();
-            }
-        }
-        /*
-        private void AddLibraryCards()
-        {
-        }
-        */
+
     }
 }
