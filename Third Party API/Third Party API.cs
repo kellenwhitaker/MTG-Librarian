edd3c9779a94bc6587941b228f5264c7f0437846
Kellen Whitaker <helenacomputers@gmail.com>
2018-12-29 21:05:10 -0700

    added skeleton code for getting tcgplayer api bearer tokens

---
+++ Third Party API/Third Party API.cs
@@ -0,0 +1,42 @@
+﻿
+using RestSharp;
+using System;
+
+namespace ThirdPartyAPI
+{
+    public static class TCGPlayer
+    {
+        private static string BearerToken = "";
+
+        private static string GetPublicKey()
+        {
+            return "";
+        }
+
+        private static string GetPrivateKey()
+        {
+            return "";
+        }
+
+        public static string GetBearerToken()
+        {
+            if (BearerToken == "")
+            {
+                string publicKey = GetPublicKey();
+                string privateKey = GetPrivateKey();
+                if (publicKey != "" && privateKey != "")
+                {
+                    var client = new RestClient("https://api.tcgplayer.com");
+                    var request = new RestRequest("token", Method.POST);
+                    request.AddParameter("grant_type", "client_credentials");
+                    request.AddParameter("client_id", publicKey);
+                    request.AddParameter("client_secret", privateKey);
+                    BearerToken = client.Execute(request).Content;
+                }
+                else
+                    throw new NotImplementedException();
+            }
+            return BearerToken;
+        }
+    }
+}
