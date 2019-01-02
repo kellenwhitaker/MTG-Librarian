5e08d5f515e656c71d10e40f78d6403ca3fbc99b
Kellen Whitaker <helenacomputers@gmail.com>
2019-01-02 13:53:02 -0700

    changed upsert method so that existing MagicCard prices are not overwritten

--- MTG Collection Tracker/Extensions/MyDbContextExtensions.cs
+++ MTG Collection Tracker/Extensions/MyDbContextExtensions.cs
@@ -12,9 +12,9 @@ namespace MTG_Librarian
         public static void Upsert(this MyDbContext context, CardSet set)
         {
             var existing = context.Sets.AsNoTracking().FirstOrDefault(x => x.Name == set.Name);
-            if (existing == null)
+            if (existing == null) // new set
                 context.Add(set);
-            else
+            else // update existing set
             {
                 context.Update(set);
             }
@@ -23,12 +23,17 @@ namespace MTG_Librarian
         public static void Upsert(this MyDbContext context, MagicCard card)
         {
             var existing = context.Catalog.AsNoTracking().FirstOrDefault(x => x.uuid == card.uuid);
-            if (existing == null)
+            if (existing == null) // new card
             {
                 context.Add(card);
             }
-            else
+            else // update existing card
             {
+                // don't overwrite existing prices
+                card.tcgplayerMarketPrice = existing.tcgplayerMarketPrice;
+                card.tcgplayerLowPrice = existing.tcgplayerLowPrice;
+                card.tcgplayerMidPrice = existing.tcgplayerMidPrice;
+                card.tcgplayerHighPrice = existing.tcgplayerHighPrice;
                 context.Update(card);
             }
         }
