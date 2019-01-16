251999f83f140b86fb6868cc9bb7b9831d3b3afa
Kellen Whitaker <helenacomputers@gmail.com>
2019-01-16 01:44:49 -0700

    fixed refresh bug

--- MTG Collection Tracker/Classes/EventManager.cs
+++ MTG Collection Tracker/Classes/EventManager.cs
@@ -130,9 +130,10 @@ namespace MTG_Librarian
                 Globals.Forms.DBViewForm.LoadSet(e.SetCode);
                 foreach (var form in Globals.Forms.OpenCollectionForms)
                 {
-                    var cardsNeedingUpdates = form.cardListView.Objects.Cast<object>().Where(x => x is FullInventoryCard card && card.SetCode == e.SetCode).Cast<FullInventoryCard>();
+                    var cardsNeedingUpdates = form.cardListView.Objects.Cast<object>().Where(x => x is FullInventoryCard card && card.SetCode == e.SetCode).Cast<FullInventoryCard>().ToList();
                     foreach (var card in cardsNeedingUpdates)
                         card.CopyFromMagicCard(Globals.Collections.MagicCardCache[card.uuid]);
+                    form.cardListView.RefreshObjects(cardsNeedingUpdates);
                 }
                 if (Globals.Forms.TasksForm.TaskManager.TaskCount == 0)
                     Globals.Forms.DBViewForm.SortCardListView();
