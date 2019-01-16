103071f430b80b5e4b6270e99c248e2788ef78da
Kellen Whitaker <helenacomputers@gmail.com>
2019-01-16 01:44:21 -0700

    fixed bug that caused incorrect encoding of json strings

--- MTG Collection Tracker/Tasks/DownloadSetTask.cs
+++ MTG Collection Tracker/Tasks/DownloadSetTask.cs
@@ -64,7 +64,7 @@ namespace MTG_Librarian
                 string mtgjsonUrl = CardSet.MTGJSONURL;
                 var zipped = DownloadZIP(CardSet.MTGJSONURL);
                 var unzipped = Unzip(zipped);
-                string json = System.Text.Encoding.Default.GetString(unzipped);
+                string json = System.Text.Encoding.UTF8.GetString(unzipped);
                 CardSet = JsonConvert.DeserializeObject<CardSet>(json);
                 if (CardSet == null) throw new InvalidDataException("Invalid JSON encountered");
                 CardSet.ScrapedName = scrapedName;
