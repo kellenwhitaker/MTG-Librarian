d4559b3a10a54a586ebe4e816a97056d382bf871
Kellen Whitaker <helenacomputers@gmail.com>
2026-06-21 02:59:24 -0600

    Add ControlExtensions with utility methods for Controls
    Added a new static class `ControlExtensions` in `ControlExtensions.cs`
    under the `MTG_Librarian` namespace. This class provides two extension
    methods for `Control` objects:
    
    - `GetChildIndex`: Retrieves the index of a control within its parent's
      `Controls` collection.
    - `SetDoubleBuffered`: Enables double buffering for a control to reduce
      rendering flicker, using reflection to set the non-public
      `DoubleBuffered` property.
    
    Included necessary namespaces `System` and `System.Windows.Forms` to
    support these methods.

---
+++ FlatButton/ControlExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MTG_Librarian
+{
+    public static class ControlExtensions
+    {
+        public static int GetChildIndex(this Control c)
+        {
+            return c.Parent.Controls.GetChildIndex(c);
+        }
+
+        public static void SetDoubleBuffered(this Control c)
+        {
+            try
+            {
+                System.Reflection.PropertyInfo aProp =
+                      typeof(System.Windows.Forms.Control).GetProperty(
+                            "DoubleBuffered",
+                            System.Reflection.BindingFlags.NonPublic |
+                            System.Reflection.BindingFlags.Instance);
+
+                aProp.SetValue(c, true, null);
+            }
+            catch (Exception ex) {  }
+        }
+    }
+}
